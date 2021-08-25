94450fd0ce5b5c3e89859a5e723c78bfd491de33
Gyanendro Kh <16163526+GyanendroKh@users.noreply.github.com>
2021-08-26 01:19:41 +0530

    Loading apps asyncronously.

--- app/src/main/java/com/gyanendrokh/alauncher/viewmodel/AppsViewModel.kt
+++ app/src/main/java/com/gyanendrokh/alauncher/viewmodel/AppsViewModel.kt
@@ -1,22 +1,23 @@
 package com.gyanendrokh.alauncher.viewmodel
 
 import android.app.Application
+import android.os.Handler
+import android.os.Looper
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
 import com.gyanendrokh.alauncher.model.AppEntity
 import com.gyanendrokh.alauncher.util.queryAllPackages
-import kotlinx.coroutines.launch
 import kotlin.math.max
 import kotlin.math.min
 
 class AppsViewModel(application: Application) : AndroidViewModel(application) {
+    private val handler = Handler(Looper.myLooper()!!)
     private val featuredAppCount = 7
     var apps = mutableStateOf<List<AppEntity>>(ArrayList())
     var featuredApps = mutableStateOf<List<AppEntity>>(ArrayList())
 
     init {
-        viewModelScope.launch {
+        handler.post {
             updateApps()
             updateFeaturedApps()
         }
