894224ea1f3e6a97dd87d29670265d32b3ba6d5c
Gyanendro Kh <16163526+GyanendroKh@users.noreply.github.com>
2021-08-25 09:37:32 +0530

    Cleaning Up.

--- app/src/main/java/com/gyanendrokh/alauncher/MainActivity.kt
+++ app/src/main/java/com/gyanendrokh/alauncher/MainActivity.kt
@@ -3,9 +3,7 @@ package com.gyanendrokh.alauncher
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.gyanendrokh.alauncher.ui.component.Greeting
 import com.gyanendrokh.alauncher.ui.theme.LauncherTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,18 +14,5 @@ class MainActivity : ComponentActivity() {
                 Greeting("Android")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LauncherTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/gyanendrokh/alauncher/ui/component/Greeting.kt
@@ -0,0 +1,9 @@
+package com.gyanendrokh.alauncher.ui.component
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
