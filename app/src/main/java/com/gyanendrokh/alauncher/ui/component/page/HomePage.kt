aa6553a6793f2b558088b329343aa008d473fc41
Gyanendro Kh <16163526+GyanendroKh@users.noreply.github.com>
2021-08-26 19:52:57 +0530

    Implementing gesture for drawing.

--- app/src/main/java/com/gyanendrokh/alauncher/ui/component/page/HomePage.kt
+++ app/src/main/java/com/gyanendrokh/alauncher/ui/component/page/HomePage.kt
@@ -2,13 +2,17 @@ package com.gyanendrokh.alauncher.ui.component.page
 
 import android.os.Handler
 import android.os.Looper
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.gyanendrokh.alauncher.model.AppEntity
@@ -19,6 +23,8 @@ import com.gyanendrokh.alauncher.util.getDateTime
 import com.gyanendrokh.alauncher.util.openApp
 
 val handler = Handler(Looper.myLooper()!!)
+
+const val offset = 50 + 20
 
 @Composable
 fun HomePage(
@@ -53,24 +59,50 @@ fun HomePage(
             modifier = Modifier.fillMaxWidth()
         )
 
-        Column(
+        Box(
             modifier = Modifier
                 .padding(vertical = 20.dp)
                 .weight(1f)
                 .fillMaxWidth(),
-            verticalArrangement = if (apps.isNotEmpty()) Arrangement.SpaceAround else Arrangement.Center
+            contentAlignment = Alignment.Center
         ) {
-            if (apps.isEmpty()) {
-                CircularProgressIndicator()
-            } else {
-                apps.forEach { app ->
-                    AppItem(
-                        app = app, onClick = {
-                            openApp(context = context, packageName = app.packageName)
+            // Using offset to align gesture for drawing to start from left of icons
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .offset(x = (-offset).dp)
+                    .pointerInput(Unit) {
+                        detectDragGestures(
+                            onDragStart = { }
+                        ) { _, _ -> }
+                    }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .offset(x = offset.dp),
+                    verticalArrangement = if (apps.isNotEmpty()) Arrangement.SpaceAround else Arrangement.Center
+                ) {
+                    if (apps.isEmpty()) {
+                        CircularProgressIndicator()
+                    } else {
+                        apps.forEach { app ->
+                            AppItem(
+                                modifier = Modifier,
+                                app = app,
+                                onClick = {
+                                    openApp(context = context, packageName = app.packageName)
+                                }
+                            )
                         }
-                    )
+                    }
                 }
             }
+
+            Canvas(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) { }
         }
 
         BottomBar(
