3c497f03c9ebc321c28d04aab630b88e547d5477
Gyanendro Kh <16163526+GyanendroKh@users.noreply.github.com>
2021-08-29 10:45:16 +0530

    Refactoring dataset.py

--- hwr/dataset.py
+++ hwr/dataset.py
@@ -9,7 +9,28 @@ IMAGE_SIZE = 28
 
 # How to decode http://yann.lecun.com/exdb/mnist/
 
-def get_images(path: str, count: int) -> np.ndarray:
+def get_mnist_image_metadata(path: str):
+    with gzip.open(path, 'r') as f:
+        f.read(4)  # magic number
+
+        a = int.from_bytes(f.read(4), 'big')
+        b = int.from_bytes(f.read(4), 'big')
+        c = int.from_bytes(f.read(4), 'big')
+
+        # (count, width, height)
+        return (a, b, c)
+
+
+def get_mnist_label_metadata(path: str):
+    with gzip.open(path, 'r') as f:
+        f.read(4)  # header
+
+        a = int.from_bytes(f.read(4), 'big')
+
+        return a
+
+
+def get_mnist_images(path: str, count: int) -> np.ndarray:
     with gzip.open(path, 'r') as f:
         f.read(16)  # header
 
@@ -19,7 +40,7 @@ def get_images(path: str, count: int) -> np.ndarray:
         return images
 
 
-def get_labels(path: str, count: int) -> np.ndarray:
+def get_mnist_labels(path: str, count: int) -> np.ndarray:
     with gzip.open(path, 'r') as f:
         f.read(8)  # header
 
@@ -35,17 +56,17 @@ def save_as_jpg(
     count: int,
     save_dir: str,
     func: Callable[[Image.Image], Image.Image] = lambda x: x,
-    label_formart: Callable[[str], str] = lambda x: x,
-    file_name_formart: Callable[[str], str] = lambda x: x,
+    label_format: Callable[[str], str] = lambda x: x,
+    file_name_format: Callable[[str], str] = lambda x: x,
 ) -> None:
-    images = get_images(img_file, count)
-    labels = get_labels(label_file, count)
+    images = get_mnist_images(img_file, count)
+    labels = get_mnist_labels(label_file, count)
 
     for idx in range(images.shape[0]):
-        n_dir = os.path.join(save_dir, label_formart(str(labels[idx])))
+        n_dir = os.path.join(save_dir, label_format(str(labels[idx])))
 
         if not os.path.exists(n_dir):
             os.makedirs(n_dir)
 
         img = Image.fromarray(images[idx])
-        func(img).save(os.path.join(n_dir, f'{file_name_formart(idx)}.jpg'))
+        func(img).save(os.path.join(n_dir, f'{file_name_format(idx)}.jpg'))
