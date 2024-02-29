a5f7aec8de926ce70f603a247d4f95532a6c71c5
Gyanendro Kh <me@gyanendrokh.dev>
2024-02-29 22:17:02 +0530

    Loading app's icon on Default dispatcher

--- app/src/main/java/com/gyanendrokh/alauncher/ui/component/AppItem.kt
+++ app/src/main/java/com/gyanendrokh/alauncher/ui/component/AppItem.kt
@@ -3,16 +3,29 @@ package com.gyanendrokh.alauncher.ui.component
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toBitmap
 import com.gyanendrokh.alauncher.model.AppEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -22,6 +35,16 @@ fun AppItem(
     onPress: (AppEntity) -> Unit = {},
     onLongPress: (AppEntity) -> Unit = {}
 ) {
+    var bitmap by remember {
+        mutableStateOf<ImageBitmap?>(null)
+    }
+
+    LaunchedEffect(Unit) {
+        withContext(Dispatchers.Default) {
+            bitmap = app.icon.toBitmap().asImageBitmap()
+        }
+    }
+
     Row(
         modifier = modifier
             .combinedClickable(
@@ -38,10 +61,18 @@ fun AppItem(
             fontSize = 20.sp
         )
 
-        Image(
-            bitmap = app.icon.toBitmap().asImageBitmap(),
-            contentDescription = app.label,
-            modifier = Modifier.size(width = 45.dp, height = 45.dp)
-        )
+        bitmap.let { bitmap ->
+            if (bitmap == null) {
+                Spacer(
+                    modifier = Modifier.size(width = 45.dp, height = 45.dp)
+                )
+            } else {
+                Image(
+                    bitmap = bitmap,
+                    contentDescription = app.label,
+                    modifier = Modifier.size(width = 45.dp, height = 45.dp)
+                )
+            }
+        }
     }
 }
