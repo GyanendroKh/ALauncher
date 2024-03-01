4ee632db1864591caf0b5338cf63661abbbdb9ed
Gyanendro Kh <me@gyanendrokh.dev>
2024-03-01 10:07:52 +0530

    Remove ConstraintLayout

--- app/src/main/java/com/gyanendrokh/alauncher/ui/page/AppsPage.kt
+++ app/src/main/java/com/gyanendrokh/alauncher/ui/page/AppsPage.kt
@@ -1,12 +1,25 @@
 package com.gyanendrokh.alauncher.ui.page
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,7 +28,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.gyanendrokh.alauncher.R
 import com.gyanendrokh.alauncher.model.AppEntity
 import com.gyanendrokh.alauncher.ui.component.AppItem
@@ -94,18 +106,12 @@ fun Header(
             )
         }
 
-        ConstraintLayout(
-            modifier = Modifier.fillMaxWidth()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.End)
         ) {
-            val (store, setting) = createRefs()
-
             IconButton(
-                modifier = Modifier
-                    .constrainAs(store) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                        end.linkTo(setting.start)
-                    },
                 onClick = onStoreClick
             ) {
                 Icon(
@@ -118,12 +124,6 @@ fun Header(
             }
 
             IconButton(
-                modifier = Modifier
-                    .constrainAs(setting) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                        end.linkTo(parent.end)
-                    },
                 onClick = onSettingsClick
             ) {
                 Icon(
@@ -136,7 +136,7 @@ fun Header(
             }
         }
 
-        Divider(
+        HorizontalDivider(
             modifier = Modifier.fillMaxWidth(),
             thickness = 0.7.dp,
             color = Color.White
