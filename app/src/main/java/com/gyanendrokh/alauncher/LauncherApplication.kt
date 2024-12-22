1b582b88967fde41532b6611af38f1fe7e42a7cb
Gyanendro Kh <me@gyanendrokh.dev>
2024-12-22 17:54:50 +0530

    Add custom UncaughtExceptionHandler

--- app/src/main/java/com/gyanendrokh/alauncher/LauncherApplication.kt
+++ app/src/main/java/com/gyanendrokh/alauncher/LauncherApplication.kt
@@ -1,5 +1,40 @@
 package com.gyanendrokh.alauncher
 
 import android.app.Application
+import android.os.Environment
+import android.widget.Toast
+import java.io.File
+import java.io.PrintStream
 
-class LauncherApplication: Application()
+class LauncherApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val defaultExceptionHandler = Thread.getDefaultUncaughtExceptionHandler()
+
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+
+            val crashLogFile = File(
+                File(
+                    Environment.getExternalStorageDirectory(),
+                    Environment.DIRECTORY_DOCUMENTS,
+                ),
+                "crash.txt"
+            )
+            PrintStream(crashLogFile).use {
+                it.println(e.message)
+                it.println()
+                e.printStackTrace(it)
+
+                Toast.makeText(
+                    this,
+                    "Crash log saved to ${crashLogFile.absolutePath}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            defaultExceptionHandler?.uncaughtException(t, e)
+        }
+    }
+}
